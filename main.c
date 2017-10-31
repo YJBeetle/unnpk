dc78d1dc4ad945c5c7b9d18355c0127096d45d5f
YJBeetle <yjbeetle@gmail.com>
2017-11-01 02:24:33 +0800

    glsl 权重放低

--- main.c
+++ main.c
@@ -166,12 +166,7 @@ int main(int argc, char **argv)
         }
         else if (strstr(file_out_type, "text"))
         {
-            if (memmem(file_out_buf, file_info[3], "vec4", 4) || memmem(file_out_buf, file_info[3], "vec2", 4) || memmem(file_out_buf, file_info[3], "tex2D", 5) || memmem(file_out_buf, file_info[3], "tex3D", 5))
-            {
-                strcat(file_out_name, ".glsl");
-                printf("| GLSL\t\t ");
-            }
-            else if (memcmp(file_out_buf, "<NeoX", 5) == 0)
+            if (memcmp(file_out_buf, "<NeoX", 5) == 0)
             {
                 strcat(file_out_name, ".NeoX.xml");
                 printf("| NeoX XML\t\t ");
@@ -195,6 +190,11 @@ int main(int argc, char **argv)
             {
                 strcat(file_out_name, ".json");
                 printf("| JSON\t\t ");
+            }
+            else if (memmem(file_out_buf, file_info[3], "vec4", 4) || memmem(file_out_buf, file_info[3], "vec2", 4) || memmem(file_out_buf, file_info[3], "tex2D", 5) || memmem(file_out_buf, file_info[3], "tex3D", 5))
+            {
+                strcat(file_out_name, ".glsl");
+                printf("| GLSL\t\t ");
             }
             else
             {
