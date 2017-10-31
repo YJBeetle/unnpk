1b6eaaaef5eb6a2861172c4dc62c054fb438ed33
YJBeetle <yjbeetle@gmail.com>
2017-11-01 02:21:42 +0800

    add .json.txt

--- main.c
+++ main.c
@@ -181,6 +181,11 @@ int main(int argc, char **argv)
                 strcat(file_out_name, ".FxGroup.xml");
                 printf("| FxGroup XML\t\t ");
             }
+            else if (file_out_buf[0] == '{' && file_out_buf[file_info[3] - 1] == '}')
+            {
+                strcat(file_out_name, ".json");
+                printf("| JSON\t\t ");
+            }
             else
             {
                 strcat(file_out_name, ".txt");
