cdcd44e126e19ef5ed902d860319e8d30a55d0f2
YJBeetle <yjbeetle@gmail.com>
2017-11-01 15:01:44 +0800

    同上

--- mapnpk.c
+++ mapnpk.c
@@ -45,20 +45,24 @@ int main(int argc, char **argv)
         fread(&file_info, 4, 7, npk);
 
         //控制台输出文件信息
-        printf("| 0x%08X\t | 0x%08X\t | %d\t | %d\t | %02X%02X%02X%02X (0x%08X)\t | %02X%02X%02X%02X (0x%08X)\t | %s\t |\n",
+        printf("| %02X%02X%02X%02X (0x%08X)\t | 0x%08X\t | %d\t | %d\t | %02X%02X%02X%02X (0x%08X)\t | %02X%02X%02X%02X (0x%08X)\t | %s\t |\n",
+               ((uint8_t *)(file_info))[0],
+               ((uint8_t *)(file_info))[1],
+               ((uint8_t *)(file_info))[2],
+               ((uint8_t *)(file_info))[3],
                file_info[0],
                file_info[1],
                file_info[2],
                file_info[3],
-               ((uint8_t *)(file_info+4))[0],
-               ((uint8_t *)(file_info+4))[1],
-               ((uint8_t *)(file_info+4))[2],
-               ((uint8_t *)(file_info+4))[3],
+               ((uint8_t *)(file_info + 4))[0],
+               ((uint8_t *)(file_info + 4))[1],
+               ((uint8_t *)(file_info + 4))[2],
+               ((uint8_t *)(file_info + 4))[3],
                file_info[4],
-               ((uint8_t *)(file_info+5))[0],
-               ((uint8_t *)(file_info+5))[1],
-               ((uint8_t *)(file_info+5))[2],
-               ((uint8_t *)(file_info+5))[3],
+               ((uint8_t *)(file_info + 5))[0],
+               ((uint8_t *)(file_info + 5))[1],
+               ((uint8_t *)(file_info + 5))[2],
+               ((uint8_t *)(file_info + 5))[3],
                file_info[5],
                file_info[6] ? "Yes" : "No");
     }
